a2d1e34493e306d38b513ec6ad1ffa099c041770
vmion <ohj106992@gmail.com>
2022-12-27 15:16:52 +0900

    _12_27_001
    MySql.Data

---
+++ DBConnectionExample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DBConnectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionInfo = "Server=localhost;Port=3306;Database=game;Uid=root;Pwd=1111";
+            using (MySqlConnection sqlcon = new MySqlConnection(connectionInfo))
+            {
+                sqlcon.Open();
+                string sql = "SELECT * FROM game.theos_characterinfo";
+                MySqlCommand cmd = new MySqlCommand(sql, sqlcon);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    int id = (int)reader["id"];
+                    string name = (string)reader["name"];
+                    string jobname = (string)reader["jobname"];
+                    byte job = reader.GetByte("job");
+                    float str = (float)reader["str"];
+                    float dex = (float)reader["dex"];
+                    float _int = reader.GetFloat("int");
+                    float con = reader.GetFloat("con");
+                    byte skill1 = reader.GetByte("skill1");
+                    byte skill2 = reader.GetByte("skill2");
+                    byte skill3 = reader.GetByte("skill3");
+                    byte skill4 = reader.GetByte("skill4");
+                    Console.WriteLine("id = " + id.ToString()+ ", name = " + name + ", jobname = " + jobname
+                        + ", job = " + job + ", str = " + str + ", dex = " + dex + ", int = " + _int + ", con = " + con
+                        + ", skill1 = " + skill1 + ", skill2 = " + skill2
+                        + ", skill3 = " + skill3 + ", skill4 = " + skill4);                    
+                }
+                reader.Close();
+                sqlcon.Close();
+            }
+        }
+    }
+}
